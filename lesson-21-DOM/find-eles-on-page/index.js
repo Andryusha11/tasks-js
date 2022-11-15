b2b0aa348fc9ab1b0305d25e4c1c50e26bd416ef
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-15 17:48:59 +0200

    add lesson-21 task 2

---
+++ lesson-21-DOM/find-eles-on-page/index.js
@@ -0,0 +1,16 @@
+export const getTitleElement = () => {
+  const title = document.querySelector('.title');
+  console.dir(title);
+  return title;
+};
+
+getTitleElement();
+
+export const getInputElement = () => {
+  const inputTypeText = document.querySelector('.task-input');
+  console.dir(inputTypeText);
+  return inputTypeText;
+};
+console.dir();
+
+getInputElement();
