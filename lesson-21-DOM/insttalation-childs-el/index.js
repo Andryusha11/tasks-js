b229838ba7c4530582c1f3eb4cf17ee7df54e1cc
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-15 20:07:50 +0200

    add less21 task6

---
+++ lesson-21-DOM/insttalation-childs-el/index.js
@@ -0,0 +1,11 @@
+export const setButton = (buttonText) => {
+  const body = document.querySelector('body');
+  console.log(body);
+  body.innerHTML = buttonText;
+};
+
+// export const setButton = (buttonText) => {
+//   const body = document.querySelector('body');
+//   console.log(body);
+//   body.textContent = buttonText;
+// };
