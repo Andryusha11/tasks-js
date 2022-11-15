bd717b36a1138fa88c943c4d175d17e43de85572
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-15 18:50:42 +0200

    update less21 task5

--- lesson-21-DOM/update-text-content/index.js
+++ lesson-21-DOM/update-text-content/index.js
@@ -2,6 +2,7 @@ export const setTitle = (text) => {
   const title = document.querySelector('.title');
   title.textContent = text;
   console.log(title);
+  return title;
 };
 
 setTitle('I am Back-End developer');
