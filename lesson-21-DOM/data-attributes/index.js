695b63173bae685bcd3601f03b3b056a0778a61f
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-17 13:47:44 +0200

    add less21 task12

---
+++ lesson-21-DOM/data-attributes/index.js
@@ -0,0 +1,6 @@
+export const squaredNumber = () => {
+  const data = document.querySelectorAll('.number');
+  const resNumber = [...data].map((el) => {
+    return (el.dataset.squaredNumber = el.dataset.number * el.dataset.number);
+  });
+};
