7884b2bdaae9c62cb40489bc507f7b290e5d7b89
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-17 12:22:02 +0200

    add less21 task8

---
+++ lesson-21-DOM/create-dom-elms-push-on-page/index.js
@@ -0,0 +1,6 @@
+export const createButton = (buttonText) => {
+  const body = document.querySelector('body');
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  body.append(button);
+};
