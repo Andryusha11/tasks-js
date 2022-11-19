206c7b698e7e965bffa162e92ed1139b9017d673
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-19 19:21:08 +0200

    change less22 task10

--- lesson-22-events-browser-handling-bubbling/subscribing-for-several-elements/index.js
+++ lesson-22-events-browser-handling-bubbling/subscribing-for-several-elements/index.js
@@ -1,28 +1,27 @@
-// const paginationPagesArr = [...document.querySelectorAll('.pagination__page')];
-
-// paginationPagesArr.map((el) => {
-//   const handleClick = (event) => {
-//     console.log(event.target.dataset.pageNumber);
-//   };
-//   el.addEventListener('click', handleClick);
-// });
-
-// console.log(paginationPagesArr);
-
-// console.log(event.target.attributes['1'].value);
-
 const paginationPagesArr = [...document.querySelectorAll('.pagination__page')];
-
-const paginationPage1 = paginationPagesArr[0];
-const paginationPage2 = paginationPagesArr[1];
-const paginationPage3 = paginationPagesArr[2];
 
 const handleClick = (event) => {
   console.log(event.target.dataset.pageNumber);
 };
 
-paginationPage1.addEventListener('click', handleClick);
-paginationPage2.addEventListener('click', handleClick);
-paginationPage3.addEventListener('click', handleClick);
+paginationPagesArr.map((el) => {
+  el.addEventListener('click', handleClick);
+});
 
 console.log(paginationPagesArr);
+
+// const paginationPagesArr = [...document.querySelectorAll('.pagination__page')];
+
+// const paginationPage1 = paginationPagesArr[0];
+// const paginationPage2 = paginationPagesArr[1];
+// const paginationPage3 = paginationPagesArr[2];
+
+// const handleClick = (event) => {
+//   console.log(event.target.dataset.pageNumber);
+// };
+
+// paginationPage1.addEventListener('click', handleClick);
+// paginationPage2.addEventListener('click', handleClick);
+// paginationPage3.addEventListener('click', handleClick);
+
+// console.log(paginationPagesArr);
