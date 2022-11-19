f528787c830c0dfb3e7f25b46561018c8814ff9b
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-19 19:25:16 +0200

    add gitignor and change less22 task10

--- lesson-22-events-browser-handling-bubbling/subscribing-for-several-elements/index.js
+++ lesson-22-events-browser-handling-bubbling/subscribing-for-several-elements/index.js
@@ -9,19 +9,3 @@ paginationPagesArr.map((el) => {
 });
 
 console.log(paginationPagesArr);
-
-// const paginationPagesArr = [...document.querySelectorAll('.pagination__page')];
-
-// const paginationPage1 = paginationPagesArr[0];
-// const paginationPage2 = paginationPagesArr[1];
-// const paginationPage3 = paginationPagesArr[2];
-
-// const handleClick = (event) => {
-//   console.log(event.target.dataset.pageNumber);
-// };
-
-// paginationPage1.addEventListener('click', handleClick);
-// paginationPage2.addEventListener('click', handleClick);
-// paginationPage3.addEventListener('click', handleClick);
-
-// console.log(paginationPagesArr);
