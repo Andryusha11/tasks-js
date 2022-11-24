e30efcb1e76743050c025b77d181ed7df8a6b59b
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-24 15:59:39 +0200

    change less24 task1

--- lesson-24/components-of-date/index.js
+++ lesson-24/components-of-date/index.js
@@ -1,11 +1,23 @@
 const nameOfDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-export const dayOfWeek = (date, days) =>
-  nameOfDays[new Date(date.setDate(date.getDate() + days)).getDay()];
+export const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
 
-const result = dayOfWeek(new Date(2019, 0, 1), 14);
+  const dayInFuture = new Date(date).setDate(day + days);
 
-const result1 = dayOfWeek(new Date(2022, 5, 23), 16);
+  return nameOfDays[new Date(dayInFuture).getDay()];
+};
+
+const date1 = new Date(2019, 0, 1);
+
+const date2 = new Date(2022, 5, 23);
+
+const result = dayOfWeek(date1, 14);
+
+const result1 = dayOfWeek(date2, 16);
 
 console.log(result);
 console.log(result1);
+
+console.log(date1);
+console.log(date2);
