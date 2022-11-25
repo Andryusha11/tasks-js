c79529109f7bcc820b418df1e49b8763f63c0e93
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-25 13:37:40 +0200

    update less24 task3

--- lesson-24/differents-of-dates/index.js
+++ lesson-24/differents-of-dates/index.js
@@ -2,25 +2,21 @@ export const getDiff = (startDate, endDate) => {
   const firstDate = new Date(startDate);
   const secondDate = new Date(endDate);
 
-  let newDate;
-  let res;
-
-  if (startDate - endDate > 0) {
-    newDate = new Date(firstDate - secondDate);
-    res = `${newDate.getDate()}d ${newDate.getHours()}h ${newDate.getMinutes()}m ${newDate.getSeconds()}s`;
-  } else {
-    newDate = new Date(secondDate - firstDate);
-    res = `${newDate.getDate()}d ${newDate.getHours()}h ${newDate.getMinutes()}m ${newDate.getSeconds()}s`;
-  }
+  const diffTime = Math.abs(new Date(firstDate - secondDate));
+  const res = `${Math.ceil(diffTime / (1000 * 60 * 60 * 24))}d ${new Date(
+    diffTime
+  ).getHours()}h ${new Date(diffTime).getMinutes()}m ${new Date(
+    diffTime
+  ).getSeconds()}s`;
 
   return res;
 };
 
-const date1 = new Date(2019, 2, 5, 2, 2, 2);
+const date1 = new Date(2019, 2, 5);
 
-const date2 = new Date(2020, 0, 1, 4, 4, 4);
+const date2 = new Date(2020, 0, 1);
 
-const date3 = new Date(2016, 44, 3, 12, 7, 22);
+const date3 = new Date(2016, 6, 3);
 
 console.log(getDiff(date1, date2));
 console.log(getDiff(date1, date3));
