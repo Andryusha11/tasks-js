a9465854d1d20d8a5200ca71bdeae7b2690a21a8
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-29 14:40:41 +0200

    add try and catch 27 1

--- lesson-27/get-data-from-locale-storage/index.js
+++ lesson-27/get-data-from-locale-storage/index.js
@@ -1,5 +1,10 @@
 export const getLocalStorageData = () =>
-  Object.entries(localStorage).reduce(
-    (acc, [key, value]) => ({ ...acc, [key]: JSON.parse(value) }),
-    {}
-  );
+  Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return { ...acc, [key]: newValue };
+  }, {});
