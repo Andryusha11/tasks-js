b98b5b458430d94863a4a41670995d6d3af26a16
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-29 14:32:46 +0200

    add json.parse 27 1

--- lesson-27/get-data-from-locale-storage/index.js
+++ lesson-27/get-data-from-locale-storage/index.js
@@ -1,5 +1,5 @@
 export const getLocalStorageData = () =>
   Object.entries(localStorage).reduce(
-    (acc, [key, value]) => ({ ...acc, [key]: value }),
+    (acc, [key, value]) => ({ ...acc, [key]: JSON.parse(value) }),
     {}
   );
