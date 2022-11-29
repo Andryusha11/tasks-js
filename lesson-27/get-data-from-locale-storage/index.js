8888af473da0ca965f99f5b2df7872c6a9716d8d
Andrew Valchuk <andryushahach1999@gmail.com>
2022-11-29 14:30:12 +0200

    update 27 1

--- lesson-27/get-data-from-locale-storage/index.js
+++ lesson-27/get-data-from-locale-storage/index.js
@@ -1,5 +1,5 @@
-export const getLocalStorageData = () => {
-  return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    return { ...acc, [key]: value };
-  }, {});
-};
+export const getLocalStorageData = () =>
+  Object.entries(localStorage).reduce(
+    (acc, [key, value]) => ({ ...acc, [key]: value }),
+    {}
+  );
