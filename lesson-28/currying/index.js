917ead093f63e1b03c41c7cd2a2a633209294eab
Andrew Valchuk <andryushahach1999@gmail.com>
2022-12-03 14:20:58 +0200

    add export 28 3

--- lesson-28/currying/index.js
+++ lesson-28/currying/index.js
@@ -1,7 +1,7 @@
 export const mult = (num1) => (num2) => num1 * num2;
 
-const twice = mult(4);
-const triple = mult(4);
+export const twice = mult(4);
+export const triple = mult(4);
 
 console.log(mult(5)(4));
 console.log(twice(2));
