d7ba6bd4d9a77e19e131717d882e61125d9b0a73
Andrew Valchuk <andryushahach1999@gmail.com>
2022-12-21 22:00:25 +0200

    add 33 1

---
+++ lesson-33/task-1/index.js
@@ -0,0 +1,26 @@
+const userAvatarImgElem = document.querySelector('.user__avatar');
+const userNameSpanElem = document.querySelector('.user__name');
+const userLocationSpanElem = document.querySelector('.user__location');
+
+const inputNameElem = document.querySelector('.name-form__input');
+const buttonNameElem = document.querySelector('.name-form__btn');
+
+const fetchUserData = (userName) => {
+  return fetch(`https://api.github.com/users/${userName}`).then((data) =>
+    data.json()
+  );
+};
+
+const renderUserData = (userData) => {
+  const { avatar_url, location, name } = userData;
+  userAvatarImgElem.src = avatar_url;
+  userNameSpanElem.textContent = name;
+  userLocationSpanElem.textContent = location;
+};
+
+const initialRequest = () => {
+  const userName = inputNameElem.value;
+  fetchUserData(userName).then((userData) => renderUserData(userData));
+};
+
+buttonNameElem.addEventListener('click', initialRequest);
