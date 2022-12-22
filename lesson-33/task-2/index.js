b60f21bb1e089b1fbf58b8ab905a168cd150e719
Andrew Valchuk <andryushahach1999@gmail.com>
2022-12-22 17:39:13 +0200

    add return 32 2

--- lesson-33/task-2/index.js
+++ lesson-33/task-2/index.js
@@ -1,9 +1,9 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 export function getTasksList() {
-  fetch(baseUrl).then((res) => res.json());
+  return fetch(baseUrl).then((res) => res.json());
 }
 
 export function getTaskById(taskId) {
-  fetch(`${baseUrl}/${taskId}`).then((res) => res.json());
+  return fetch(`${baseUrl}/${taskId}`).then((res) => res.json());
 }
