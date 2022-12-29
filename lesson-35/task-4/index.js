8858b2bd837c0f298a514fe138412cf895761bc4
Andrew Valchuk <andryushahach1999@gmail.com>
2022-12-29 19:53:30 +0200

    change 35 4

--- lesson-35/task-4/index.js
+++ lesson-35/task-4/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // put your code here
-export const parseUser = (JSONstring) => {
+export const parseUser = (jsonString) => {
   try {
-    return JSON.parse(JSONstring);
+    return JSON.parse(jsonString);
   } catch (err) {
     return null;
   }
