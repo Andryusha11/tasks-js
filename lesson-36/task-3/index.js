d83b07fca57176fe5d4fbca5330a2846034046ce
Andrew Valchuk <andryushahach1999@gmail.com>
2022-12-30 18:18:45 +0200

    remove test 36 3

--- lesson-36/task-3/index.js
+++ lesson-36/task-3/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * @param {string[]} users
- * @return {promise}
- */
-
 const getUsersBlogs = async (users) => {
   // put your code here
   const resData = await Promise.all(
@@ -21,9 +16,3 @@ const getUsersBlogs = async (users) => {
   );
   return resData;
 };
-
-// examples
-getUsersBlogs(['google', 'facebook', 'reactjs']).then((linksList) =>
-  console.log(linksList)
-); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-getUsersBlogs(['microsoft']).then((linksList) => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
